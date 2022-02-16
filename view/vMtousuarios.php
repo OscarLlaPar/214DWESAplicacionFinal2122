a78c5a736744ade2cc2a1f68bd8fa1284188b9c7
oscar <oscar.llapar@educa.jcyl.es>
2022-02-16 11:29:27 +0100

    16/02/2022 Reseteo vista mtoUsuarios

--- view/vMtousuarios.php
+++ view/vMtousuarios.php
@@ -2,11 +2,40 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>OLP-Aplicación Final - MtoUsuarios</title>
+        <link href="webroot/css/mtoUsuarios.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <main class="mainUsuarios">
+            <form action="index.php" class="formulario">
+                <button class="boton" name="volver">Volver</button>
+                <label>Buscar usuario por descripción:</label>
+                <div>
+                    <input type="text" name="busqueda" id="busqueda" placeholder="Buscar...">
+                    <button type="button" id="buscar" name="buscar" class="boton">Buscar</button>
+                </div>
+                
+            </form>
+            <div id="container">
+                <table class="tablaUsuarios">
+                    <thead>
+                        <tr>
+                            <td>Nombre</td>
+                            <td>Descripción</td>
+                            <td>Última conexión</td>
+                            <td>Conexiones</td>
+                            <td>Perfil</td>
+                            <td>Imagen</td>
+                            <td>Eliminar</td>
+                        </tr>
+                    </thead>
+                    <tbody  id="usuarios">
+                        
+                    </tbody>
+                </table>
+            </div>
+        </main>
+        <script src="webroot/js/mtoUsuarios.js">
+        </script>
     </body>
 </html>
